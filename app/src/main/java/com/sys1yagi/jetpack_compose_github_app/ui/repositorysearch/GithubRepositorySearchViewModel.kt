0e70f6801f9f7609bccd4bc36e900743d15ed36a
Toshihiro Yagi <sylc.yagi@gmail.com>
2019-10-26 14:06:28 +0900

    Remove LiveDataExtension

--- app/src/main/java/com/sys1yagi/jetpack_compose_github_app/ui/repositorysearch/GithubRepositorySearchViewModel.kt
+++ app/src/main/java/com/sys1yagi/jetpack_compose_github_app/ui/repositorysearch/GithubRepositorySearchViewModel.kt
@@ -5,14 +5,13 @@ import androidx.lifecycle.*
 import com.sys1yagi.jetpack_compose_github_app.infrastructure.api.GithubRepositorySearchResult
 import com.sys1yagi.jetpack_compose_github_app.infrastructure.repository.GithubRepositoryRepository
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class GithubRepositorySearchViewModel(
     private val githubRepositoryRepository: GithubRepositoryRepository
 ) : ViewModel() {
 
-    val state = State()
+    val state = GithubRepositorySearchState()
 
     fun setQuery(query: String) {
         viewModelScope.launch {
@@ -47,7 +46,7 @@ sealed class GithubRepositorySearchViewState {
 }
 
 @Model
-class State {
+class GithubRepositorySearchState {
     var viewState: GithubRepositorySearchViewState = GithubRepositorySearchViewState.ShowContent
     var githubRepositorySearchResult: GithubRepositorySearchResult? = null
 }
